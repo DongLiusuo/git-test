97e82ae541e3185ae813ba5e3c03b4542e635129
DongLiusuo <535425450@qq.com>
2020-04-03 13:24:33 +0800

    init

---
+++ src/org/example/git/FirstClass.java
@@ -0,0 +1,11 @@
+package org.example.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+    }
+}
